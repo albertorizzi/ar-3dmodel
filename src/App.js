5b5f0fb86bf2054edc28ab82e3a729f595ddc640
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-10 18:50:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { ARCanvas, ARMarker } from '@artcom/react-three-arjs/lib/ar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-50">
+        Hello world! ciao cjsss
+      </h1>
+      <ARCanvas
+        gl={{ antialias: false, powerPreference: "default", physicallyCorrectLights: true }}
+        onCameraStreamReady={() => console.log("Camera stream ready")}
+        onCameraStreamError={() => console.error("Camera stream error")}
+        onCreated={({ gl }) => {
+          gl.setSize(window.innerWidth, window.innerHeight)
+        }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 0]} intensity={10.0} />
+        <ARMarker
+          params={{ smooth: true }}
+          type={"pattern"}
+          patternUrl={"data/patt.hiro"}
+          onMarkerFound={() => {
+            console.log("Marker Found")
+          }}>
+
+        </ARMarker>
+      </ARCanvas>
     </div>
+
   );
 }
 
