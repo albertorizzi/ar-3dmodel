dbdfe16a4a7b699d4d73e94b09119df68c834051
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-20 16:08:13 +0200

    comment in header

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import ARcomponents from './components/ARcompoents';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold bg-blue-300 p-4 rounded-lg text-center">
+      {/* <h1 className="text-3xl font-bold bg-blue-300 p-4 rounded-lg text-center">
         Progetto di Architetture multimediali <br />Realizzazione di un prototipo di visualizzazione di modelli 3D in AR<br /><span className='text-base'>Alberto Rizzi</span>
-      </h1>
+      </h1> */}
 
       <ARcomponents />
     </div>
